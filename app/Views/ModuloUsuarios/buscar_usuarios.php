39771a6eded096ff0553a023e33e0c005f516694
ProyectoTecnoparque <proyectostecnoparquepereira@gmail.com>
2021-09-13 07:45:21 -0500

    up

--- app/Views/ModuloUsuarios/buscar_usuarios.php
+++ app/Views/ModuloUsuarios/buscar_usuarios.php
@@ -103,7 +103,27 @@
 
   function iniciar() {
     $(".desactivar").click(inactivarusuario);
-    $('#usuarios_activos').DataTable();
+    $('#usuarios_activos').DataTable({
+       "language": {
+        "url": "//cdn.datatables.net/plug-ins/1.10.22/i18n/Spanish.json"
+      },
+      "responsive": true,
+      "autoWidth": false,
+      "ordering": true,
+      "aoColumnDefs": [{
+          'bSortable': false,
+          'aTargets': [4]
+        },
+        {
+          'bSortable': false,
+          'aTargets': [6]
+        },
+        {
+          'bSortable': false,
+          'aTargets': [7]
+        }
+      ],
+    });
   }
 
   function inactivarusuario() {
